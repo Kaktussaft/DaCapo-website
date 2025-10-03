976f9150519a55770a78908074270e364316510b
benjamin <aeplibenjamin@gmail.com>
2025-10-03 15:57:34 +0200

    landing pageHolders

--- src/pages/aboutUs/landing-page/landing-page.ts
+++ src/pages/aboutUs/landing-page/landing-page.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../../../components/navigation/navigation";
 import { Footer } from "../../../components/footer/footer";
+import { CustomFontHeader } from "../../../components/custom-font-header/custom-font-header";
 @Component({
   selector: 'app-landing-page',
-  imports: [NavigationComponent, Footer],
+  imports: [NavigationComponent, Footer, CustomFontHeader],
   templateUrl: './landing-page.html',
-  styleUrl: './landing-page.css'
+  styleUrls: ['./landing-page.css']
 })
 export class LandingPage {
 
