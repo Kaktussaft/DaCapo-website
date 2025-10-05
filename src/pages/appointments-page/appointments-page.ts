a5f020c8e2531e137aed33ca5105fdf2928914dd
benjamin <aeplibenjamin@gmail.com>
2025-10-05 06:53:41 +0200

    add appointments page

--- src/pages/appointments-page/appointments-page.ts
+++ src/pages/appointments-page/appointments-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../../components/navigation/navigation";
 import { Footer } from "../../components/footer/footer";
+import { CustomFontHeader } from '../../components/custom-font-header/custom-font-header';
 
 @Component({
   selector: 'app-appointments-page',
-  imports: [NavigationComponent, Footer],
+  imports: [NavigationComponent, Footer, CustomFontHeader],
   templateUrl: './appointments-page.html',
   styleUrl: './appointments-page.css'
 })
