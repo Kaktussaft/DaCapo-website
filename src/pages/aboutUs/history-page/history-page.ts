c1966174803719c551466e6105d63fd4439c0325
benjamin <aeplibenjamin@gmail.com>
2025-10-05 06:44:40 +0200

    Add History page

--- src/pages/aboutUs/history-page/history-page.ts
+++ src/pages/aboutUs/history-page/history-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Footer } from "../../../components/footer/footer";
 import { NavigationComponent } from "../../../components/navigation/navigation";
+import { CustomFontHeader } from '../../../components/custom-font-header/custom-font-header';
 
 @Component({
   selector: 'app-history-page',
-  imports: [Footer, NavigationComponent],
+  imports: [Footer, NavigationComponent, CustomFontHeader],
   templateUrl: './history-page.html',
   styleUrl: './history-page.css'
 })
