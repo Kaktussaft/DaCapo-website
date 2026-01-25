938f6ec359519e61e191495f4f419b817d9271bc
benjamin <aeplibenjamin@gmail.com>
2026-01-25 10:34:10 +0100

    adjustments

--- src/pages/contact-page/contact-page.ts
+++ src/pages/contact-page/contact-page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { NavigationComponent } from "../../components/navigation/navigation";
+import { FormsModule } from '@angular/forms';
+import { NavigationComponent } from '../../components/navigation/navigation';
 
 @Component({
   selector: 'app-contact-page',
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, FormsModule],
   templateUrl: './contact-page.html',
-  styleUrl: './contact-page.css'
+  styleUrl: './contact-page.css',
 })
 export class ContactPage {
+  name: string = '';
+  message: string = '';
 
+  sendEmail() {
+    if (this.name && this.message) {
+      const subject = encodeURIComponent(`Kontaktanfrage von ${this.name}`);
+      const body = encodeURIComponent(
+        `Name: ${this.name}\n\nNachricht:\n${this.message}`,
+      );
+      window.location.href = `mailto:jugendchor.dacapoalfine@gmail.com?subject=${subject}&body=${body}`;
+    } else {
+        alert('Bitte Name und Nachricht eingeben.');
+    }
+  }
 }
