563d4bf96a67949f87027c6318e9157739f1bec4
benjamin <aeplibenjamin@gmail.com>
2026-02-01 17:52:17 +0100

    fix issue where navigating didn't go to the top of the site

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, inject, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { HttpHeaders, provideHttpClient } from '@angular/common/http';
@@ -12,7 +12,11 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes, 
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
+      withViewTransitions()
+    ),
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
     provideApollo(() => {
